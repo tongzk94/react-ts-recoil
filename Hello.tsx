ae438d6cd2c1e34aa98fa978378d91dc12061251
tongzk94 <tongzk365@163.com>
2020-10-14 13:59:01 +0800

    2 files modified, 1 added

--- Hello.tsx
+++ Hello.tsx
@@ -1,3 +1,20 @@
-import React from 'react';
+import React from "react";
+import { useRecoilState } from "recoil";
+import { textState } from "./stateManager";
 
-export default ({ name }) => <h1>Hello {name}!</h1>;
+function Hello() {
+  const [text, setText] = useRecoilState(textState);
+  const onChange = e => {
+    setText(e.target.value);
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      <span>text: {text}</span>
+    </div>
+  );
+}
+
+export default Hello;
--- index.tsx
+++ index.tsx
@@ -1,29 +1,19 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+import { RecoilRoot } from 'recoil';
 import Hello from './Hello';
 import './style.css';
 
-interface AppProps { }
-interface AppState {
-  name: string;
-}
-
-class App extends Component<AppProps, AppState> {
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      name: 'React'
-    };
   }
 
   render() {
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>
-          Start editing to see some magic happen :)
-        </p>
-      </div>
+      <RecoilRoot>
+        <Hello />
+      </RecoilRoot>
     );
   }
 }
